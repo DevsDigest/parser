07a616ea52b7f47f30cb516b70fc15fd7dc8d669
Tiago Celestino <tcelestino@gmail.com>
2017-10-20 01:38:56 -0200

    working in the project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dev Digest Worker
+
+Get all set off content the websites
+
+Soon...
---
+++ data/feeds.js
@@ -0,0 +1,24 @@
+const feeds = [
+  {
+    url: 'https://www.smashingmagazine.com/feed/',
+    category: 'development'
+  },
+  {
+    url: 'https://www.sitepoint.com/feed/',
+    category: 'development'
+  },
+  {
+    url: 'https://css-tricks.com/feed/',
+    category: 'css'
+  },
+  {
+    url: 'https://scotch.io/feed/',
+    category: 'development'
+  },
+  {
+    url: 'https://davidwalsh.name/feed',
+    category: 'development'
+  }
+];
+
+module.exports = feeds;
