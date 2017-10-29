c8f69c987b6966a23d76d64d7d79d9ebeade7593
Tiago Celestino <tcelestino@gmail.com>
2017-10-29 13:23:53 -0200

    settings environment vars

--- infra/database.js
+++ infra/database.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const config = require('./config');
 const db = mongoose.connection;
 
-db.openUri(config.MONGODB_URI);
+db.openUri(config.mongo);
 
-db.on('connected', console.log.bind(console, 'Mongoose default connection open to ' + config.MONGODB_URI));
+db.on('connected', console.log.bind(console, 'Mongoose default connection open to ' + config.mongo));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('disconnected', console.log.bind(console, 'Mongoose default connection disconnected'));
 
